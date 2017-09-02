3c793fa6c349e071d4c0a9146a61293476a61c83
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2017-09-02 20:15:22 +0400

    firmware.ino: Update

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -10,8 +10,8 @@
 #include "OLEDDisplayUi.h"
 
 // Initialize the OLED display using Wire library
-const int PIN_SDA = 5; // IO 17
-const int PIN_SCL = 4; // IO 18
+const int PIN_SDA = 18; // IO 17
+const int PIN_SCL = 17; // IO 18
 SSD1306 display(0x3c, PIN_SDA, PIN_SCL);
 
 OLEDDisplayUi ui(&display);
@@ -23,8 +23,9 @@ static const int SCREEN_H = 48;
 int clockCenterX = SCREEN_W;
 int clockCenterY = ((SCREEN_H - 16) / 2) + 6;
 
-const int ECHO_PIN = 12;
-const int TRIG_PIN = 13;
+/* Led */
+static const int LED_PIN_1 = 13;
+static const int LED_PIN_2 = 14;
 
 // utility function for digital clock display: prints leading 0
 static String twoDigits(int digits) {
@@ -49,20 +50,18 @@ void digitalClockFrame(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t 
   display->setFont(ArialMT_Plain_24);
   display->drawString(clockCenterX + x, clockCenterY + y, timenow);
 
-  if (second() % 2) {
-    cm = getDistance();
-  }
+ // if (second() % 2) {
+//    cm = getDistance();
+ // }
 
-  String centimeters;
-  if (cm > 80) {
-    centimeters = "80+ cm";
-  } else {
-    centimeters = twoDigits(cm) + "  cm";
-  }
+//  String centimeters;
+//  if (cm > 80) {
+//    centimeters = "80+ cm";
+//  } else {
+//    centimeters = twoDigits(cm) + "  cm";
+//  }
 
-  display->setFont(ArialMT_Plain_16);
-  display->drawString(clockCenterX + x, clockCenterY + y + 20,
-                      centimeters);
+//
 }
 
 FrameCallback frames[] = { digitalClockFrame };
@@ -97,10 +96,10 @@ void setup() {
   Serial.begin(115200);
   Serial.println();
   // pinMode(23, OUTPUT);
-  // digitalWrite(23, LOW);
-
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
+  pinMode(LED_PIN_1, OUTPUT);
+  pinMode(LED_PIN_2, OUTPUT);
+  digitalWrite(LED_PIN_1, HIGH);
+  digitalWrite(LED_PIN_2, LOW);
 
   attachInterrupt(0, handleButton, FALLING);
 
@@ -110,25 +109,6 @@ void setup() {
 
   setTime(calculateEpoch());
 
-}
-
-long microsecondsToCentimeters(long microseconds) {
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the
-  // object we take half of the distance travelled.
-  return microseconds / 29 / 2;
-}
-
-long getDistance() {
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(TRIG_PIN, LOW);
-
-  long duration = pulseIn(ECHO_PIN, HIGH);
-
-  return microsecondsToCentimeters(duration);
 }
 
 void loop() {
